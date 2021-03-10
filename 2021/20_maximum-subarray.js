d2595ee21720b278584db4eb09ed0c853e0ffe7a
zhaolong <naokimidori@163.com>
2021-03-10 23:19:27 +0800

    feat: maximum-subarray

---
+++ 2021/20_maximum-subarray.js
@@ -0,0 +1,42 @@
+/* 
+  最大子序和
+  给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+   
+  示例 1：
+    输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+    输出：6
+    解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+
+  示例 2：
+    输入：nums = [1]
+    输出：1
+
+  示例 3：
+    输入：nums = [0]
+    输出：0
+
+  示例 4：
+    输入：nums = [-1]
+    输出：-1
+
+  示例 5：
+    输入：nums = [-100000]
+    输出：-100000
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxSubArray = function(nums) {
+  let arr = [...nums];
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i-1] > 0) {
+          arr[i] += arr[i-1];
+      }
+  }
+  return Math.max(...arr);
+};
+
+// 参考：
+// https://leetcode-cn.com/problems/maximum-subarray/solution/zui-da-zi-xu-he-by-leetcode-solution/
